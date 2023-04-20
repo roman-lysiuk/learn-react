8aa87c2c0f5b58bd28fedc04377f486b2b20fc24
Roman Lysiuk <gekalo1025@gmail.com>
2023-04-20 20:29:33 +0300

    refactor: add render app

---
+++ src/components/RenderApp/RenderApp.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import Router from '../../components/Router/Router';
+import '../../index.scss';
+
+function RenderApp() {
+  return <Router />;
+}
+
+export default RenderApp;
