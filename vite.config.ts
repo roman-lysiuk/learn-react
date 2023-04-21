ef1b0f80bce78a1bda6a15dd88b276dec4ab94a8
Roman Lysiuk <gekalo1025@gmail.com>
2023-04-21 16:54:30 +0300

    feat :add ssr setting

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,8 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-
   test: {
     globals: true,
     environment: 'jsdom',
@@ -16,4 +14,5 @@ export default defineConfig({
       reporter: 'text',
     },
   },
+  ssr: { noExternal: ['@reduxjs/toolkit'] },
 });
