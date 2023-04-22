3bb1f1a72e3cf47afdf3bfe589699a49a7bf9260
Roman Lysiuk <gekalo1025@gmail.com>
2023-04-22 11:01:14 +0300

    feat: add type options for stream

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -5,7 +5,21 @@ import { Provider } from 'react-redux';
 import React from 'react';
 import { ssrStore } from './store/store';
 
-export default function ServerAppRender(url: string, options: {}) {
+type Options = {
+  identifierPrefix?: string;
+  namespaceURI?: string;
+  nonce?: string;
+  bootstrapScriptContent?: string;
+  bootstrapScripts?: Array<string>;
+  bootstrapModules?: Array<string>;
+  progressiveChunkSize?: number;
+  onShellReady?: () => void;
+  onShellError?: () => void;
+  onAllReady?: () => void;
+  onError?: (error: unknown) => void;
+};
+
+export default function ServerAppRender(url: string, options: Options = {}) {
   const store = ssrStore();
   const stream = renderToPipeableStream(
     <Provider store={store}>
