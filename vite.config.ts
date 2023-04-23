9da5c0a4728017b2bde2746a7b29404f4b8dea07
Roman Lysiuk <gekalo1025@gmail.com>
2023-04-23 16:15:28 +0300

    feat: add rimraf and change script

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite';
+import { configDefaults } from 'vitest/config';
 import react from '@vitejs/plugin-react';
 import EnvironmentPlugin from 'vite-plugin-environment';
 import istanbul from 'vite-plugin-istanbul';
 
 export default defineConfig({
+  build: { sourcemap: true },
   plugins: [
     react(),
     istanbul({
@@ -20,7 +22,9 @@ export default defineConfig({
     coverage: {
       provider: 'c8',
       reporter: 'text',
+      skipFull: true,
     },
+    exclude: [...configDefaults.exclude, 'instrumented'],
   },
   ssr: { noExternal: ['@reduxjs/toolkit'] },
 });
