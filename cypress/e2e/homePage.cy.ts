1eefee1f7ab2b7d9c65c90785d05eead9ee5edb2
Roman Lysiuk <gekalo1025@gmail.com>
2023-04-23 16:15:43 +0300

    feat: add e2e tests

---
+++ cypress/e2e/homePage.cy.ts
@@ -0,0 +1,99 @@
+/// <reference types="Cypress" />
+describe('Home page', () => {
+  it('Visits the localhost', () => {
+    cy.visit('http://localhost:3000/');
+  });
+
+  it('finds the content "Search"', () => {
+    cy.visit('http://localhost:3000/');
+    cy.contains('Search');
+  });
+
+  it('clicks the link "About us"', () => {
+    cy.visit('http://localhost:3000/');
+    cy.contains('About us').click();
+  });
+
+  it('clicking "About us" navigates to a new url', () => {
+    cy.visit('http://localhost:3000/');
+
+    cy.contains('About us').click();
+
+    cy.url().should('include', '/about');
+  });
+
+  it('Search by name Frodo"', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('.search').type('Frodo');
+    cy.contains('Search').click();
+    cy.get('.character-list').children().should('have.length', 2);
+    cy.contains('Frodo Gardner');
+    cy.contains('Frodo Baggins').click();
+  });
+
+  it('Click by name Frodo for view detail modal"', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('.search').type('Frodo');
+    cy.contains('Search').click();
+    cy.contains('Frodo Baggins').click();
+    cy.contains('Race: Hobbit');
+    cy.contains('Gender: Male');
+    cy.contains('Birth - 22 September ,TA 2968');
+    cy.contains('Birth - 22 September ,TA 2968');
+    cy.contains('Hair: Brown');
+    cy.contains('wikiUrl: http://lotr.wikia.com//wiki/Frodo_Baggins');
+    cy.contains('Death - Unknown (Last sighting ,September 29 ,3021,) (,SR 1421,)');
+  });
+  it('Open and close modal"', () => {
+    cy.visit('http://localhost:3000/');
+    cy.get('.search').type('Frodo');
+    cy.contains('Search').click();
+    cy.contains('Frodo Baggins').click();
+    cy.get('div').should('have.class', 'modal').and('have.class', 'active');
+    cy.get('.close-icon img').click();
+  });
+  it('api latency check and Loader', () => {
+    cy.visit('http://localhost:3000');
+    cy.intercept('http://localhost:3000', { delay: 2000 }).as('getData');
+    cy.get('.loader');
+    cy.wait(1000);
+    cy.get('body').not('.loader');
+  });
+  it('Error Api Message', () => {
+    cy.intercept('GET', 'https://the-one-api.dev/v2/character/?limit=1000', (req) => {
+      req.reply({
+        statusCode: 404,
+      });
+    }).as('getData');
+    cy.visit('http://localhost:3000');
+    cy.wait('@getData');
+    cy.contains('Error: loading character cards');
+    cy.contains('Nothing found');
+  });
+  it('Error Api Message for search id', () => {
+    cy.intercept('GET', 'https://the-one-api.dev/v2/character/5cd99d4bde30eff6ebccfbbe', (req) => {
+      req.reply({
+        statusCode: 404,
+      });
+    }).as('getData');
+
+    cy.visit('http://localhost:3000');
+    cy.contains('Adanel').click();
+    cy.wait('@getData');
+    cy.get('div').should('have.class', 'modal').and('have.class', 'active');
+    cy.contains('Error: loading character card');
+  });
+  it('Error Api Message for search name', () => {
+    cy.intercept('GET', 'https://the-one-api.dev/v2/character/?name=/Adanel/i', (req) => {
+      req.reply({
+        statusCode: 404,
+      });
+    }).as('getData');
+    cy.visit('http://localhost:3000');
+    cy.get('.search').type('Adanel');
+    cy.contains('Search').click();
+    cy.wait('@getData');
+    cy.contains('Error: loading character cards');
+    cy.contains('Nothing found');
+  });
+});
